5e74a61c0aa3d8bd613c6d3482e02122a1bfd057
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2021-02-11 09:46:15 -0300

    test

--- middlewares/validateUser.js
+++ middlewares/validateUser.js
@@ -6,13 +6,16 @@ const alreadyExistsErr = { message: 'Email already registered' };
 
 const regex = /^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
 
-const quickFixComplexityError = (name, email, password) => (!!((!name || !email || !password)));
+const quickFixComplexityError = (email, password, name) => {
+  if (!email || !password || !name) return true;
+  return false;
+};
 
 const validateUser = rescue(async (req, res, next) => {
   const { name, email, password } = req.body;
-
   // if (!name || !email || !regex.test(email) || !password) return res.status(400).json(invalidErr);
-  if (quickFixComplexityError(name, email, password)) return res.status(400).json(invalidErr);
+  if (quickFixComplexityError(email, password, name)) return res.status(400).json(invalidErr);
+  if (!regex.test(email)) return res.status(400).json(invalidErr);
 
   if (!regex.test(email)) return res.status(401).json(invalidErr);
 
